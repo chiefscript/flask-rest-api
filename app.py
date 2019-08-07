e6f6b628a83344c42aaffd64ec2e515528ae1d19
chiefscript <dennis.munene@outlook.com>
2019-08-07 11:30:52 +0300

    Initialize database

--- app.py
+++ app.py
@@ -4,3 +4,21 @@ from flask_marshmallow import Marshmallow
 
 import os
 
+# Init
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init Marshmallow
+marsh = Marshmallow(app)
+
+# Run
+if __name__ == '__main__':
+    app.run(debug=True)
+
